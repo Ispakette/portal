bd9d2424cb60ff1014aeba10e7151aa9712ee7f1
Ispakette <ispakette@gmail.com>
2023-03-04 10:09:40 +0100

    portal

---
+++ Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+	{
+//		Debug.Log("Collision");
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/GameOverText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverText : MonoBehaviour
+{
+
+    public GameObject capsule;
+    private Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        // start text off as completely transparent black
+        text.color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (capsule == null)       
+        {
+            // reveal text only when helicopter is null (destroyed)
+            text.color = new Color(237, 255, 230, 1);
+        }
+    }
+}
